50968cf6d6c35bb597263f553fc17b850d1659e6
Daniel Rizal <flipinteract@gmail.com>
2014-04-09 14:08:29 +1000

    Add Controller

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
-function MyController ($scope) {
-	$scope.clock = {
-		now: new Date()
-	};
-
-	var updateClock = function () {
-		$scope.clock.now = new Date ()
-	};
-
-	setInterval (function () {
-		$scope.$apply(updateClock);
-	}, 1000) ;
-
-	updateClock();
-};
+angular.module("myApp", [])
+	.controller('MyController', function($scope){
+		$scope.name ="Daniel";
+		
+	});
